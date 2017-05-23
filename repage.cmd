2670983868dd62331b58dec3fe5d07292932719b
iamblocksberg <blocksberg.dw@gmail.com>
2017-05-23 11:59:50 +0700

    Add files via upload
    change name variable

--- repage.cmd
+++ repage.cmd
@@ -1,8 +1,8 @@
 @echo off
 title = Repage by iamblocksberg;
 
-set loadTime=5
-set url=www.watklangworawihan.com
+set timeout=5
+set url="http://www.google.com"
 set chrome="C:\Program Files (x86)\Google\Chrome\Application\"
 set count=1
 
@@ -10,7 +10,7 @@ set count=1
 echo. & echo. & echo.
 echo Round: %count%
 start /D %chrome% chrome.exe -incognito %url%
-timeout %loadTime%
+timeout %timeout%
 taskkill /F /IM chrome.exe /T
 set /A count=%count% + 1
 goto :loop
